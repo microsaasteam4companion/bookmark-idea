63fe722d55a418e2b15a172b400bc3dffc7deea6
tirth patel <tirth646@gmail.com>
2026-03-26 02:49:07 +0530

    Final Build: Dodo Payments Integration, License Activation, and Live Site Fix

---
+++ landing-page/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDo8dRr4WICkFoYEhtRTxi0EDd5dgdJ9_Q",
+  authDomain: "ctrlsense-e1310.firebaseapp.com",
+  projectId: "ctrlsense-e1310",
+  storageBucket: "ctrlsense-e1310.firebasestorage.app",
+  messagingSenderId: "339634652508",
+  appId: "1:339634652508:web:9d06216b3f964d1fdf6d99",
+  measurementId: "G-5HDL6RGT0L"
+};
